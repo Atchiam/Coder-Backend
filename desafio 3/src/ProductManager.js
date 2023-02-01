76ba94476c9c2787dd2478ee30fa6e860da0d33f
Atchiam <braian.ferreyra@hotmail.com>
2023-02-01 18:01:55 -0300

    desafio 3

---
+++ desafio 3/src/ProductManager.js
@@ -0,0 +1,103 @@
+import {promises as fs} from "fs"
+
+class ProductManager {
+    constructor(path) {
+        this.path = path;
+    }
+
+    async addProduct (titulo, descripcion, precio, imagen, stock, code) {
+        try{
+            let valid  = [titulo, descripcion, precio, imagen, stock, code]
+            const read = await fs.readFile(this.path, "utf8");
+            const data = JSON.parse(read);
+            const objCode = data.find((product) => product.code == code);
+
+            if(objCode){
+                throw error;
+            }else{
+                if(valid.includes(null)||valid.includes("")||valid.includes(undefined)){
+                    console.log("Todos los campos deben estar completos");
+                }else{
+                    let id;
+                    id = data.length + 1;
+                    let nuevoProducto = new Product(titulo, descripcion, precio, imagen, stock, code, id);
+                    data.push(nuevoProducto);
+                    await fs.writeFile(this.path, JSON.stringify(data), "utf-8");
+                    
+                }
+            }
+        }catch (error){
+            console.log("El code del producto ya se encuentra en uso" + error);
+        };
+    }
+
+    async getProducts() {
+        try {
+        const read = await fs.readFile(this.path, "utf8");
+        return (JSON.parse(read)); 
+        } catch (error) {
+        throw error;
+        }
+    }
+
+    async getProductByID(id) {
+        try {
+        const read = await fs.readFile(this.path, "utf-8");
+        const data = JSON.parse(read);
+        const product = data.find((product) => product.id === id);
+        if (product) {
+            return (product);
+        } else {
+            return ("No se encontro el producto");
+        }
+        } catch (error) {
+        throw error;
+        }
+    }
+
+    async deleteProduct(id) {
+        try {
+        const read = await fs.readFile(this.path, "utf-8");
+        const data = JSON.parse(read);
+        const newData = data.filter((product) => product.id !== id);
+        await fs.writeFile(this.path, JSON.stringify(newData), "utf-8");
+        return console.log(
+            `El producto ha sido eliminado exitosamente`
+        );
+        } catch (error) {
+        throw error;
+        }
+    }
+
+    async updateProduct(id, titulo, descripcion, precio, imagen, stock, code) {
+        const read = await fs.readFile(this.path, "utf-8");
+        const data = JSON.parse(read);
+        if (data.some(producto => producto.id === id)){
+            let indice = data.findIndex(producto => producto.id === id)
+            data[indice].title      = titulo
+            data[indice].description= descripcion
+            data[indice].price      = precio
+            data[indice].thumbnail  = imagen
+            data[indice].code       = code
+            data[indice].stock      = stock
+            await fs.writeFile(this.path, JSON.stringify(data), "utf-8");
+        }else{
+            console.log("producto no encontrado");
+        }
+
+    }
+}
+
+class Product {
+    constructor(titulo, descripcion, precio, imagen, stock, code, id) {
+        this.title = titulo;
+        this.description = descripcion;
+        this.price = precio;
+        this.thumbnail = imagen;
+        this.code = code;
+        this.id = id;
+        this.stock = stock;
+    }
+}
+
+export default ProductManager;
---
+++ desafio 3/src/index.js
@@ -0,0 +1,35 @@
+import ProductManager from "./ProductManager.js";
+import express from "express";
+
+const app  = express();
+const PORT = 4000;
+const manager= new ProductManager("./src/productos.json");
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req,res)=>{
+    res.send("Desafio numero 3, server con express "+ PORT)
+});
+
+app.get("/catalogo", async (req,res)=>{
+    const products = await manager.getProducts();
+    let limit  =parseInt(req.query.limit);
+    let data;
+    console.log(limit)
+    if (limit) {
+        if(limit < 0 || limit > products.length){
+            data=`el limite debe ser un numero positivo y menor al numero ${products.length}`
+        }else{data = products.slice(0, limit);}
+    } else {
+        data = products;
+    }
+    res.send(data);
+});
+
+app.get("/catalogo/:id", async(req,res)=>{
+    const product = await manager.getProductByID(parseInt(req.params.id))
+    product === null ? res.send("el producto que ingresaste no existe"): res.send(product)
+});
+
+app.listen(PORT,()=>{
+    console.log(`servidor en "localhost:${PORT}"`)
+});
